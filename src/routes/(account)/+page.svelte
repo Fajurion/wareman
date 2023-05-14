3fa6a2fbf9e1c99ba3b7e215b93c6e5e51c89c50
Julian <70802809+Unbreathable@users.noreply.github.com>
2023-05-14 19:58:25 +0200

    Logo and start

---
+++ src/routes/(account)/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import logo from "$lib/assets/fajurion.png";
+</script>
+
+<div class="title">
+    <img src={logo} alt="Fajurion Logo" />
+    <h1>Wareman</h1>
+</div>
+
+<style lang="scss">
+
+    .title {
+        display: flex;
+        align-items: center;
+
+        img {
+            filter: brightness(100%) sepia(100) saturate(100) hue-rotate(120deg);
+            height: 50px;
+            aspect-ratio: 1/1;
+        }
+    }
+
+</style>
